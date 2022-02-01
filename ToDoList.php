4523213461edcaabc821c90c87f42effa1e385c8
jfr <jfrsimbare@gmail.com>
2022-02-01 10:04:27 +0100

    ExoToDoList

---
+++ ToDoList.php
@@ -0,0 +1,17 @@
+<?php {
+    class Todo
+    {
+        public function __construct(public string $title, public string $description, public string $done_at)
+        {
+        }
+        public function setDone(){
+
+        }
+        public function getDone(){
+
+        }
+        public function unDone(){
+
+        }
+    }
+}
